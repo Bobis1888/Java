5edfd783acb0cc04e272a10864172a18aa2ff673
Bobis1888 <bobis1888@gmail.com>
2019-06-02 20:57:49 +0500

    new_stage

---
+++ src/Transmogrify/Starship.java
@@ -0,0 +1,49 @@
+package Transmogrify;
+
+class Ship{
+    public void doIt(){
+        System.out.println("I`m ready to fly");
+    }
+}
+class RunShip extends Ship{
+    public void doIt(){
+        System.out.println("I can fly");
+    }
+}
+class StopShip extends Ship{
+    public void doIt(){
+        System.out.println("Stop machine");
+    }
+}
+class SoSShip extends Ship{
+    public void doIt() {
+        System.out.println("SoS,Sos,SoS");
+    }
+}
+class StageShip{
+    private Ship ship = new Ship();
+    public void runShip(){
+        ship = new RunShip();
+    }
+    public void stopShip(){
+        ship = new StopShip();
+    }
+    public void sos(){
+        ship = new SoSShip();
+    }
+    public void radio(){
+        ship.doIt();
+    }
+}
+public class Starship {
+    public static void main(String[] args) {
+        StageShip starStageShip = new StageShip();
+        starStageShip.radio();
+        starStageShip.runShip();
+        starStageShip.radio();
+        starStageShip.stopShip();
+        starStageShip.radio();
+        starStageShip.sos();
+        starStageShip.radio();
+    }
+}
