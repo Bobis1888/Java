7b4babdfe1b1289fb04988817e65cf3f3c735ed5
bobis1888 <bobis1888@gmail.com>
2019-07-13 20:44:23 +0500

    6

--- src/MyP/Regex/Mains.java
+++ src/MyP/Regex/Mains.java
@@ -1,25 +1,16 @@
 package MyP.Regex;
 
-import java.util.*;
 import java.util.regex.*;
 
 public class Mains {
     public static void main(String[] args) {
-        String findmatches = "asdads asd asd as das12e 13e 32 12r 4rd w5 w5";
+        Pattern p = Pattern.compile("<.+?>");
+        Matcher m = p.matcher("<asd>asasd<asd> asdasd <asdasd>");
 
-    Pattern p = Pattern.compile("[a-zA-Z]");
-    Matcher m = p.matcher(findmatches);
-    Set<String> uniqueString = new TreeSet<>();
-    while (m.find()){
-        uniqueString.add(m.group());
-    }
-        System.out.println("Found unique Strings: " + uniqueString);
-    Pattern p1 = Pattern.compile("\\d");
-    Matcher m1 = p1.matcher(findmatches);
-    Set<Integer> uniqueInteger = new TreeSet<>();
-    while (m1.find()){
-        uniqueInteger.add(Integer.valueOf(m1.group()));
-    }
-        System.out.println("Found unique Integer: " + uniqueInteger);
+        while (m.find()){
+
+            System.out.println(m.start() + " " + m.group() + " ");
+
+        }
     }
 }
--- src/MyP/Swing/Main.java
+++ src/MyP/Swing/Main.java
@@ -2,6 +2,9 @@ package MyP.Swing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main {
     public static JFrame getFrame(){
@@ -13,8 +16,48 @@ public class Main {
         jFrame.setBounds(dimension.width/2 - 250,dimension.height/2 - 150,500,300);
         return jFrame;
     }
+
+
+
+    static class MyComponent extends JComponent{
+        protected void paintComponent(Graphics g){
+            Font font = new Font("Ubuntu",Font.BOLD,20);
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setFont(font);
+            g2.drawString("My first desktop program!",40,20);
+            Point2D p1 = new Point2D.Float(40,30);
+            Point2D p2 = new Point2D.Float(200,30);
+            Line2D l2 = new Line2D.Float(40,30,300,30);
+            g2.draw(l2);
+            Ellipse2D e1 = new Ellipse2D.Float(70,70,170,170);
+            g2.setPaint(Color.GREEN);
+            g2.fill(e1);
+            g2.draw(e1);
+            Rectangle2D r2 = new Rectangle2D.Float(270,170,70,70);
+            g2.draw(r2);
+            g2.setPaint(Color.CYAN);
+            g2.fill(r2);
+            try {
+                URL url = new URL("https://upload.wikimedia.org/wikipedia/ru/b/b1/Java_Runtime_Environment.png");
+                Image image= new ImageIcon(url).getImage();
+                g2.drawImage(image,220,50,null);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+
+            //String[]fonts=GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+            //for (String s:fonts){
+            //  System.out.println(s);
+            //}
+        }
+    }
+
+
     public static void main(String[] args) {
         JFrame jFrame = getFrame();
-        Font font = new Font("MyProgram",Font.BOLD,20);
+        jFrame.add(new MyComponent());
+
     }
 }
