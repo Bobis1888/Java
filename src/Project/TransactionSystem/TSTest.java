c35a67d5948de6b211933d647b524f1c06bfcb0f
bobis1888 <bobis1888@gmail.com>
2019-08-01 20:43:04 +0500

    TransActionSystemRefactoring add log

--- src/Project/TransactionSystem/TSTest.java
+++ src/Project/TransactionSystem/TSTest.java
@@ -4,18 +4,29 @@ import Project.TransactionSystem.Data.BankAccount;
 import Project.TransactionSystem.Data.MapData;
 
 import java.util.Map;
+import java.util.Scanner;
 
 public class TSTest {
     public static void main(String[] args) {
-        Map<Integer, BankAccount> mapsData = MapData.getMapData(50);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size mapData: =>>> ");
+        int size = scanner.nextInt();
+        Map<Integer, BankAccount> mapsData = MapData.getMapData(size);
+        System.out.println("Select sender bank account number: =>>> ");
+        int numberSends = scanner.nextInt();
+        System.out.println("Select beneficiary`s bank account number: =>>> ");
+        int numberGetter = scanner.nextInt();
         try {
-            System.out.println(mapsData.get(20).getBalance());
+            System.out.println(mapsData.get(numberSends).getBalance());
+            System.out.println(mapsData.get(numberGetter).getBalance());
         }catch (NullPointerException e){
-            System.out.println("Not founds");
+            System.out.println("Not found");
         }
-        System.out.println(mapsData.get(21).getBalance());
-        TransactionSystem.transaction(mapsData,50,31,50);
-        System.out.println(mapsData.get(20).getBalance());
-        System.out.println(mapsData.get(21).getBalance());
+        System.out.println("Enter sum transaction: =>>> ");
+        int sum = scanner.nextInt();
+        TransactionSystem.transaction(mapsData,numberSends,numberGetter,sum);
+        System.out.println("*****");
+        System.out.println(mapsData.get(numberSends).getBalance());
+        System.out.println(mapsData.get(numberGetter).getBalance());
     }
 }
