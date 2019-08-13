20e51bb52df492031dda14386451b7903c4e1b79
bobis1888 <bobis1888@gmail.com>
2019-08-14 01:51:47 +0500

    coding

---
+++ src/Study/TenToTwentyChapter/generics/Ex20.java
@@ -0,0 +1,27 @@
+package Study.TenToTwentyChapter.generics;
+interface TypeInterface1{
+    void test();
+    void test2();
+}
+class Type1 implements TypeInterface1{
+    public void test(){
+        System.out.println("Test");
+    }
+    public void test2(){
+        System.out.println("Test2");
+    }
+    public void test3(){
+        System.out.println("Test3");
+    }
+}
+public class Ex20 {
+    public static  <Type1 extends TypeInterface1>void testEx20(Type1 t){
+        t.test();
+        t.test2();
+    }
+
+    public static void main(String[] args) {
+        Type1 type1 = new Type1();
+        testEx20(type1);
+    }
+}
---
+++ src/Study/TenToTwentyChapter/generics/ShipConteiner.java
@@ -0,0 +1,74 @@
+package Study.TenToTwentyChapter.generics;
+
+import net.mindview.util.Generator;
+
+import java.util.ArrayList;
+
+class Container{
+    private static long counter = 1;
+    private final long id = counter++;
+    private long rowNumber;
+    private String description;
+    private long lvlNumber;
+    private Container(String description){
+        this.description = description;
+    }
+    public void setRowNumber(long rowNumber){
+        this.rowNumber = rowNumber;
+    }
+    public void setLevelNumber(long lvlNumber){
+        this.lvlNumber = lvlNumber;
+    }
+    public String toString(){return description + " RowNumber: " + rowNumber + " LVLNumber: " + lvlNumber + " ID: " + id;}
+    public static Generator<Container> generator = new Generator<>() {
+        @Override
+        public Container next(){
+            return new Container("Container: ");
+        }
+    };
+}
+class Level extends ArrayList<Container>{
+    private static long counter = 1;
+    private final long id = counter++;
+    public Level(int size,long rowNumber){
+        Generators.fill(this,Container.generator,size);
+        for (Container c : this){
+            c.setLevelNumber(id);
+            c.setRowNumber(rowNumber);
+        }
+    }
+}
+
+class Row extends ArrayList<Level> {
+    private static long counter = 1;
+    private final  long id = counter++;
+
+    public Row(int sizeRow,int size){
+        for (int i = 0; i < sizeRow ; i++) {
+            add(new Level(size,id));
+        }
+    }
+}
+
+public class ShipConteiner extends ArrayList<Row>{
+    public ShipConteiner(int nRow,int nLevel,int container){
+        for (int i = 0; i < nRow; i++) {
+            add(new Row(nLevel,container));
+        }
+        }
+        public String toString(){
+            StringBuilder result = new StringBuilder();
+            for (Row r : this)
+                for (Level l : r)
+                    for (Container c : l){
+                        result.append(c);
+                        result.append("\n");
+                    }
+            return result.toString();
+        }
+
+    public static void main(String[] args) {
+        System.out.println(new ShipConteiner(10,4,10));
+    }
+    }
+
