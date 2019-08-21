badf0ba24dca81ecc6c573cb66a1a9651d296510
bobis1888 <bobis1888@gmail.com>
2019-08-22 01:16:14 +0500

    Test

--- src/Test/TestAlgorithm/FastSort.java
+++ src/Test/TestAlgorithm/FastSort.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 
 /**@author bobis1888 */
 public class FastSort {
-    private static int[] fastSort(int [] arr){
+    private static void fastSort(int [] arr){
         if (arr.length<2){
-            return arr;
+            System.out.println(Arrays.toString(arr));
+            return;
         }
 
     }
 
     public static void main(String[] args) {
         int [] ints = new int[]{2,4,1,6,4};
-        System.out.println(Arrays.toString(fastSort(ints)));
+        fastSort(ints);
     }
 }
