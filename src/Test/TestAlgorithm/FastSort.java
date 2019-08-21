33a1fe718d9e272b72027f025da66a15cb736c54
bobis1888 <bobis1888@gmail.com>
2019-08-22 00:36:25 +0500

    Test

--- src/Test/TestAlgorithm/FastSort.java
+++ src/Test/TestAlgorithm/FastSort.java
@@ -8,26 +8,8 @@ public class FastSort {
     private static int[] fastSort(int [] arr){
         if (arr.length<2){
             return arr;
-        }else {
-            int medium = arr[arr.length / 2];
-            ArrayList<Integer> lowIfMedium = new ArrayList<>();
-            ArrayList<Integer> upIfMedium = new ArrayList<>();
-            for (int i = 0; i < arr.length; i++) {
-                if (medium <= arr[i]) {
-                    lowIfMedium.add(i);
-                } else {
-                    upIfMedium.add(i);
-                }
-            }
-            int[] lowNumber = new int[lowIfMedium.size()];
-            int[] uprNumber = new int[upIfMedium.size()];
-            for (int i = 0; i < lowIfMedium.size(); i++) {
-                lowNumber[i] = lowIfMedium.get(i);
-            }
-            for (int i = 0; i < upIfMedium.size(); i++) {
-                uprNumber[i] = upIfMedium.get(i);
-            }
-            }
+        }
+
     }
 
     public static void main(String[] args) {
