6b11b3fa46e8563e3936a30bf4644de1c65ddd94
bobis1888 <bobis1888@gmail.com>
2019-08-21 23:28:02 +0500

    Test

---
+++ src/Test/TestAlgorithm/FastSort.java
@@ -0,0 +1,13 @@
+package Test.TestAlgorithm;
+/**@author bobis1888 */
+public class FastSort {
+    private int[] fastSort(int [] arr){
+        if (arr.length<2){
+            return arr;
+        }else {
+            int medium = arr.length / 2;
+            int lowMedium =
+
+        }
+    }
+}
