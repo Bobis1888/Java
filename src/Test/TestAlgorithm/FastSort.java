7ec0d0fba1ab3de2cec792b976a7e0f4186d66f0
bobis1888 <bobis1888@gmail.com>
2019-08-22 15:02:29 +0500

    Test

--- src/Test/TestAlgorithm/FastSort.java
+++ src/Test/TestAlgorithm/FastSort.java
@@ -4,28 +4,12 @@ import java.util.Arrays;
 
 /**@author bobis1888 */
 public class FastSort {
-
-    private static Integer [] fastSort(Integer[] arr){
-        if (arr.length<2) {
+    public static Integer[] fastSort(Integer [] arr, int start , int end){
+        if (arr.length<2){
             return arr;
-        }else if (arr.length == 2){
-            int buffArrItem;
-            if (arr[0]>arr[1]){
-                buffArrItem = arr[0];
-                arr[0] = arr[1];
-                arr[1] = buffArrItem;
-                return arr;
-            }
-        } return qSort(arr);
-    }
-    private static Integer [] qSort(Integer[] integers){
+        }else {
 
-        return integers;
-    }
-
-
-    public static void main(String[] args) {
-        Integer [] integers = new Integer[]{4, 2, 8, 1, 3, 7, 6, 5};
-        System.out.println(Arrays.toString(fastSort(integers)));
+        }
     }
 }
+
