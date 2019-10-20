43f2b325c34d123941fa005324b5c59c070a1399
bobis1888 <bobis1888@gmail.com>
2019-10-21 02:22:41 +0500

    new version Sea War

--- src/project/SeaBattle/Core.java
+++ src/project/SeaBattle/Core.java
@@ -1,22 +1,57 @@
 package project.SeaBattle;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 class Core {
-    public ArrayList<String> getList(){
+    private LinkedList<ArrayList<String>> ships;
+    Core(){
+        ships = new LinkedList<>();
+        ships.add(getCoordinate());
+        System.out.println("Created one Ship");
+    }
+    Core(int number){
+        ships = new LinkedList<>();
+        System.out.println("Created " + number + " Ships");
+        for (int i = 0; i < number ; i++) {
+            ships.add(getCoordinate());
+        }
+    }
+    private ArrayList<String> getCoordinate(){
         List<String> listChar = Arrays.asList("A","B","C","D","E","F","G","H","J","K");
         Random random = new Random();
         int randomChar = random.nextInt(8);
         int randomInteger = random.nextInt(8);
+        boolean change = random.nextBoolean();
         ArrayList<String> shipCoordinate = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             shipCoordinate.add(listChar.get(randomChar) + randomInteger);
-            randomChar++;
-            randomInteger++;
+            if (change)
+                randomChar++;
+            else
+                randomInteger++;
         }
         return shipCoordinate;
+    }
+    void game(){
+        System.out.println(ships);
+        int counter = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (!ships.isEmpty()){
+            String scannerString = scanner.nextLine().toUpperCase();
+            counter++;
+            for (ArrayList<String> coordinateShip : ships) {
+                if (coordinateShip.contains(scannerString)){
+                    coordinateShip.remove(scannerString);
+                    System.out.println("Hit!!!");
+                    if (coordinateShip.isEmpty()){
+                        System.out.println("You Killed Ship!!!");
+                        ships.remove(coordinateShip);
+                    }
+                }else {
+                    System.out.println("You Missed!");
+                }
+            }
+        }
+        System.out.println("You counter: " + counter);
     }
 }
--- src/project/SeaBattle/Main.java
+++ src/project/SeaBattle/Main.java
@@ -1,28 +1,9 @@
 package project.SeaBattle;
 
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Core core = new Core();
-        ArrayList<String> shipCoordinate = core.getList();
-        System.out.println(shipCoordinate);
-        int counter = 0;
-        Scanner scanner = new Scanner(System.in);
-        while (!shipCoordinate.isEmpty()) {
-            String scannerString = scanner.nextLine().toUpperCase();
-            if (shipCoordinate.contains(scannerString)){
-                shipCoordinate.remove(scannerString);
-                System.out.println("Hit");
-                if (shipCoordinate.isEmpty()){
-                    System.out.println("Sos!");
-                    System.out.println("Yuo win counter: " + ++counter);
-                }
-            }else {
-                System.out.println("Past");
-            }
-            counter++;
-        }
-
+        Core core = new Core(3);
+        core.game();
     }
 }
